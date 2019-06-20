da2f26613b13e2bd9701f2601eeeda2a97c2e144
fedepvg <federicopvg@gmail.com>
2019-06-20 14:49:43 -0300

    movement added

---
+++ Parcial2-DVJ2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    const float MaxLeftRotation = 90;
+    const float MaxRightRotation = 270;
+    const float HalfRotation = 180;
+
+    public float ForceMultiplier;
+    public float TorqueMultiplier;
+    float MovementInput;
+    float RotationInput;
+    Rigidbody2D PlayerRigidody;
+
+    private void Start()
+    {
+        PlayerRigidody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        MovementInput = Input.GetAxis("Vertical");
+        RotationInput = - Input.GetAxisRaw("Horizontal");
+
+        if (MovementInput < 0)
+            MovementInput = 0;
+
+        PlayerRigidody.AddRelativeForce(Vector2.up * ForceMultiplier * MovementInput);
+
+        if (RotationInput != 0)
+            PlayerRigidody.AddTorque(RotationInput * TorqueMultiplier);
+        else
+            PlayerRigidody.angularVelocity = 0;
+        
+        Vector3 EulerRotation = transform.eulerAngles;
+        if (EulerRotation.z > MaxLeftRotation && EulerRotation.z < HalfRotation)
+            EulerRotation.z = MaxLeftRotation;
+        else if (EulerRotation.z < MaxRightRotation && EulerRotation.z > HalfRotation)
+            EulerRotation.z = MaxRightRotation;
+        transform.eulerAngles = EulerRotation;
+    }
+
+    void Update()
+    {
+        
+    }
+}
