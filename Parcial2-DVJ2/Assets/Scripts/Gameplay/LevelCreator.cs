4ee1e28ee528571d5ba372217060cacbf9c12695
fedepvg <federicopvg@gmail.com>
2019-06-23 17:15:20 -0300

    level generated automatically

---
+++ Parcial2-DVJ2/Assets/Scripts/Gameplay/LevelCreator.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour
+{
+    Bounds LevelBounds;
+    const int LevelDistance = 100;
+    LineRenderer LineRend;
+    EdgeCollider2D EdgeCollider;
+    
+    int MaxHeight = 0;
+    int MinHeight = -3;
+    float MaxWidth;
+    float MinWidth;
+    int WidthMultiplier = 4;
+    float MaxYAddition = 1f;
+    float MinYAddition = 0.5f;
+
+    public Collider2D Player;
+    
+    int PointCount = 0;
+    List<Vector2> Points;
+    float XPosition;
+    float YPosition;
+    float PrevYPosition;
+    int NotLandingPlatformPoints;
+    public int MaxNotLandingPlatformPoints;
+    public int MinNotLandingPlatformPoints;
+
+    void Start()
+    {
+        Points = new List<Vector2>();
+        LevelBounds = CameraUtils.OrthographicBounds();
+        LineRend = GetComponent<LineRenderer>();
+        EdgeCollider = GetComponent<EdgeCollider2D>();
+        MinWidth = Player.bounds.size.x;
+        MaxWidth = MinWidth * WidthMultiplier;
+        NotLandingPlatformPoints = 0;
+        LineRend.positionCount = 1;
+        Points.Add(GetFirstPoint());
+        LineRend.SetPosition(PointCount, Points[PointCount]);
+        GenerateLevel();
+    }
+
+    void GenerateLevel()
+    {
+        while(Vector2.Distance(Points[0], Points[PointCount]) < LevelDistance)
+        {
+            Points.Add(GetPoint());
+            PointCount++;
+            LineRend.positionCount++;
+            LineRend.SetPosition(PointCount, Points[PointCount]);
+        }
+        EdgeCollider.points = Points.ToArray();
+    }
+
+    Vector2 GetPoint()
+    {
+        Vector2 newPoint;
+        XPosition += Random.Range(MaxWidth, MinWidth);
+
+        if (!IsLandingPlatform())
+        {
+            float randY = Random.Range(MinYAddition, MaxYAddition) * SignMultiplier();
+            YPosition += randY;
+            YPosition = Mathf.Clamp(YPosition,MinHeight, MaxHeight);
+            if(PrevYPosition == YPosition)
+            {
+                YPosition -= randY;
+            }
+            NotLandingPlatformPoints++;
+            PrevYPosition = YPosition;
+        }
+        newPoint = new Vector2(XPosition, YPosition);
+
+        return newPoint;
+    }
+
+    int SignMultiplier()
+    {
+        int sign = Random.Range(0, 2);
+        if(sign==0)
+        {
+            return -1;
+        }
+        return 1;
+    }
+
+    bool IsLandingPlatform()
+    {
+        float rand = Random.Range(0f, 1f);
+        if(rand < GetExponentialProbability(NotLandingPlatformPoints))
+        {
+            NotLandingPlatformPoints = 0;
+            return true;
+        }
+        return false;
+    }
+
+    float GetExponentialProbability(int currentValue)
+    {
+        float valuesCount = MaxNotLandingPlatformPoints - MinNotLandingPlatformPoints;
+        float maxChance = Mathf.Pow(2, valuesCount);
+        float currentValueChance = Mathf.Pow(2, currentValue - MinNotLandingPlatformPoints);
+
+        return currentValueChance / maxChance;
+    }
+
+    Vector2 GetFirstPoint()
+    {
+        XPosition = CameraUtils.OrthographicBounds().min.x;
+        float randHeight = Random.Range(MinHeight, MaxHeight);
+        return new Vector2(XPosition, randHeight);
+    }
+}
